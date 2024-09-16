6b2178fb11a92dde14e3a0c084a4cc00685e1133
soyeon <102381857+soyeon1806@users.noreply.github.com>
2024-09-17 03:56:51 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.04 ms, Memory: 82.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(String my_string, String is_suffix) {
+        int answer = 0;
+        
+        if (is_suffix.length() <= my_string.length()){
+            if (my_string.substring(my_string.length() - is_suffix.length()).equals(is_suffix)){
+                answer = 1;
+            }
+        }
+        
+        return answer;
+    }
+}
