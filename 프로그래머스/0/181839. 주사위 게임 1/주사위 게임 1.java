09752d47da887bc87c35412a6e36f9b989719846
soyeon <102381857+soyeon1806@users.noreply.github.com>
2024-09-18 03:38:35 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.08 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.java
@@ -0,0 +1,19 @@
+import java.lang.Math;
+
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 0;
+        
+        if (a % 2 == 0 && b % 2 == 0){
+           answer = Math.abs(a - b); 
+        }
+        else if(a % 2 != 0 && b % 2 != 0){
+            answer = (int)Math.pow(a, 2) + (int)Math.pow(b, 2);
+        }
+        else{
+            answer = 2 * (a + b);
+        }
+        
+        return answer;
+    }
+}
