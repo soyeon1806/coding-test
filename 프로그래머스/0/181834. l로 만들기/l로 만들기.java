71ac995830e4bfeda9208b57f73f5f9067b0255f
soyeon <102381857+soyeon1806@users.noreply.github.com>
2024-09-18 03:47:29 +0900

    [level 0] Title: l로 만들기, Time: 22.41 ms, Memory: 84.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+        
+        answer = myString.replaceAll("[a-k]", "l");
+        
+        return answer;
+    }
+}
