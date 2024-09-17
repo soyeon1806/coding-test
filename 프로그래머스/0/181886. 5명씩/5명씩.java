4ba30c2434a401b9ba49ac87932677b32a4bd000
soyeon <102381857+soyeon1806@users.noreply.github.com>
2024-09-18 07:04:23 +0900

    [level 0] Title: 5명씩, Time: 0.05 ms, Memory: 78.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181886. 5명씩/5명씩.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public String[] solution(String[] names) {
+        List<String> answer = new ArrayList<>();
+        
+        for(int i = 0; i < names.length; i += 5){
+            answer.add(names[i]);
+        }
+        
+        return answer.toArray(new String[answer.size()]);
+    
+    }
+}
