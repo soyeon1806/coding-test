7c4d26db90c50e23eb88a6ac8d0500e4fe4c44ac
soyeon <102381857+soyeon1806@users.noreply.github.com>
2024-09-18 07:22:54 +0900

    [level 0] Title: 할 일 목록, Time: 0.05 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public String[] solution(String[] todo_list, boolean[] finished) {
+        List<String> answer = new ArrayList<>();
+        
+        for(int i = 0; i < todo_list.length; i++){
+            if(!finished[i]){
+                answer.add(todo_list[i]);
+            }
+        }
+        
+        
+        return answer.toArray(new String[answer.size()]);
+    }
+}
