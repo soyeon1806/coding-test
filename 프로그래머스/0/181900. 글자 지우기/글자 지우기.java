62011653cdb6f73329698191fce71c929466bc70
soyeon <102381857+soyeon1806@users.noreply.github.com>
2024-09-28 12:54:35 +0900

    [level 0] Title: 글자 지우기, Time: 2.20 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string, int[] indices) {
+        String answer = "";
+        
+        String[] tmp = my_string.split("");
+        
+        for(int i = 0; i < indices.length; i++){
+            tmp[indices[i]] = "";
+        }
+        
+        for(String str : tmp){
+            answer += str;
+        }
+        
+        return answer;
+    }
+}
