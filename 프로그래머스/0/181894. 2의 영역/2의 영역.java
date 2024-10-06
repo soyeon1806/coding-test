e8c0d73fa9321183933cb43f025a4a6ee00f3299
soyeon <102381857+soyeon1806@users.noreply.github.com>
2024-10-06 10:41:58 +0900

    [level 0] Title: 2의 영역, Time: 1.90 ms, Memory: 103 MB -BaekjoonHub

---
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[] solution(int[] arr) {        
+        int start = -1, end = -1;
+        
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 2) {
+                start = i;
+                break;
+            }
+        }
+        
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (arr[i] == 2) {
+                end = i;
+                break;
+            }
+        }
+        
+        if (start == -1 || end == -1) {
+            return new int[] {-1};
+        }
+        
+        int[] answer = new int[end - start + 1];
+        for (int i = start; i <= end; i++) {
+            answer[i - start] = arr[i];
+        }
+        
+        return answer;
+    }
+}
