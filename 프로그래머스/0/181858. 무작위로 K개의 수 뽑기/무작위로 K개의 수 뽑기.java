2a259b9eeba0d4b23f93599847ec726839d1f2e9
soyeon <102381857+soyeon1806@users.noreply.github.com>
2024-10-06 12:01:04 +0900

    [level 0] Title: 무작위로 K개의 수 뽑기, Time: 29.74 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr, int k) {
+        List<Integer> answer = new ArrayList<>();
+        
+        for(int i = 0;i < arr.length; i++){
+            if(answer.size() == k){
+                break;
+            }
+            else if(!answer.contains(arr[i])){
+                answer.add(arr[i]);
+            }
+        }
+        
+        while(answer.size() != k){
+            answer.add(-1);
+        }
+        
+        return answer.stream().mapToInt(i -> i).toArray();
+    }
+}
